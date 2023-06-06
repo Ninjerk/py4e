117628cd5cb12838ece0cc9ada45fb9a5bd908df
Brant Thorpe <ninjerk@gmail.com>
2023-06-05 22:04:04 -0400

    Updated addconf.py, last digit is still getting cut off at this point

--- addconf.py
+++ addconf.py
@@ -1,2 +1,17 @@
 fname = input('Enter file name:')
 fhand = open(fname)
+total = 0
+
+def numSlice(slicee):
+    startIndex = slicee.find(' ')
+    lastIndex = slicee.find(' ',startIndex+1)
+    addend = slicee[startIndex+1:lastIndex]
+    print(addend)
+    return addend
+	
+for line in fhand:
+    line = line.strip()
+    if line.startswith('X-DSPAM-Confidence:'):
+            numSlice(line)
+            print(line)
+    #print(line)
